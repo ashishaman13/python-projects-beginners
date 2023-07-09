0ac3d718cb3ab73522ee07bac37db8a84b51686e
ASHISH AMAN <ashish@Cloudtrades-MacBook-Pro.local>
2023-07-09 12:38:53 +0530

    added score in quiz game

--- quiz_game.py
+++ quiz_game.py
@@ -8,38 +8,48 @@ if playing.lower() != "yes":
 
 print("Okay! Fasten your seat belts")
 
+score = 0
+
 question1 = input("What is the full form of CPU? ")
 if question1.lower() == "central processing unit":
     print('Correct!')
+    score += 1
 
 else:
     print('Try Next Time!')
 
 
 question2 = input("What is the full form of GPU? ")
-if question2 == "Graphical Processing Unit":
+if question2.lower() == "graphical processing unit":
     print('Correct!')
+    score += 1
 
 else:
     print('Try Next Time!')
 
 question3 = input("What is the full form of IAS? ")
-if question3 == "Indian Administrative Service":
+if question3.lower() == "indian administrative service":
     print('Correct!')
+    score += 1
 
 else:
     print('Try Next Time!')
 
 question4 = input("What is the full form of AWS? ")
-if question4 == "Amazon Web Service":
+if question4.lower() == "amazon web service":
     print('Correct!')
+    score += 1
 
 else:
     print('Try Next Time!')
 
 question5 = input("What is the full form of SAA? ")
-if question5 == "Solution Architect Assosciate":
+if question5.lower() == "solution architect assosciate":
     print('Correct!')
+    score += 1
 
 else:
     print('Try Next Time!')
+
+print(f"Your score is : {score}/5")
+
