c01805e48d3f68366c97bbeb18e54c547e5c162b
ASHISH AMAN <ashish@Cloudtrades-MacBook-Pro.local>
2023-07-09 12:00:41 +0530

    added question

--- quiz_game.py
+++ quiz_game.py
@@ -1,3 +1,18 @@
 print("Welcome to my computer quizz!")
 
-playing = 
+playing = input("Do you want to play ? ")
+print(playing)
+
+if playing != "yes":
+    quit()
+
+print("Okay! Fasten your seat belts")
+
+question1 = input("What is the full form of CPU? ")
+if question1 == "Central Processing Unit":
+    print('Correct!')
+
+else:
+    print('Try Next Time!')
+
+
