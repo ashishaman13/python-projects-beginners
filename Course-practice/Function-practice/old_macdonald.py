2dbfe3b5d5a040729ba0ec2c92bfd94f006b9012
ashishaman13 <ashish34gupta@gmail.com>
2025-07-01 14:07:37 +0530

    added new file

---
+++ Course-practice/Function-practice/old_macdonald.py
@@ -0,0 +1,10 @@
+"""
+OLD MACDONALD: Write a function that capitalizes the first and fourth letters of a name
+
+old_macdonald('macdonald') --> MacDonald
+Note: 'macdonald'.capitalize() returns 'Macdonald'
+"""
+
+
+def capitol(name):
+    retu
