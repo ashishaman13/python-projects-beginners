8da7f71a230d8f12757f01928b78af9a94819434
ashishaman13 <ashish34gupta@gmail.com>
2025-07-01 13:21:00 +0530

    added new file

---
+++ Course-practice/Function-practice/makes_twenty.py
@@ -0,0 +1,27 @@
+"""
+MAKES TWENTY: Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
+makes_twenty(20,10) --> True
+makes_twenty(12,8) --> True
+makes_twenty(2,3) --> False
+"""
+
+
+"""
+ Method 1
+def makes_twenty(a,b):
+    if a+b ==20 or a == 20 or b == 20:
+        return True
+    else:
+        return False
+
+result = makes_twenty(12,8)
+print(result)
+ """
+
+
+def makes_twenty(a,b):
+    return a+b == 20 or a == 20 or b == 20
+
+result = makes_twenty(2,3)
+print(result)
+
